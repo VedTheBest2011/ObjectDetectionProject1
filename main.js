8b3b37ccb83aa0d8f4f4e4031d019e14dea18e57
VedTheBest2011 <82335213+VedTheBest2011@users.noreply.github.com>
2022-02-05 19:39:32 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function redirect_living_room() {
+    window.location("living_room.html")
+}
+function redirect_kitchen() {
+    window.location('kitchen.html')
+}
+function redirect_study_table() {
+    window.location("bunk_bed.html")
+}
+function redirect_bunk_bed() {
+    window.location("bunk_bed.html")
+}
